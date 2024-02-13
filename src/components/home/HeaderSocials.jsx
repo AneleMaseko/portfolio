8a41df6e9423b95bc0bf9b28cecf9a74ebd5bfed
AneleMaseko <anelemaseko5@gmail.com>
2024-02-13 14:10:10 +0200

    social links

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -4,23 +4,23 @@ import react from 'react'
 const HeaderSocials = () => {
     return (
        <div className="home__socials">
-        <a href="" className="home__social-link" target="_blank">
+        {/* <a href="" className="home__social-link" target="_blank">
         <i class="fa-brands fa-instagram"></i>
         </a>
 
         <a href="" className="home__social-link" target="_blank">
         <i class="fa-brands fa-twitter"></i>
-        </a>
+        </a> */}
 
-        <a href="" className="home__social-link" target="_blank">
+        <a href="https://github.com/AneleMaseko" className="home__social-link" target="_blank">
         <i class="fa-brands fa-github"></i>
         </a>
 
-        <a href="" className="home__social-link" target="_blank">
+        <a href="https://www.facebook.com/anele.maseko.56/" className="home__social-link" target="_blank">
         <i class="fa-brands fa-facebook"></i>
         </a>
 
-        <a href="" className="home__social-link" target="_blank">
+        <a href="https://linkedin.com/in/anele-maseko-0763b2224" className="home__social-link" target="_blank">
         <i class="fa-brands fa-linkedin"></i>
         </a>
        </div>
