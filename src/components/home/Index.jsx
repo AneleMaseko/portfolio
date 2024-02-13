439b2f5b7abf53ca4cffa8e9940302e08e8bcec3
AneleMaseko <anelemaseko5@gmail.com>
2024-02-13 14:16:18 +0200

    hire me button

--- src/components/home/Index.jsx
+++ src/components/home/Index.jsx
@@ -16,7 +16,7 @@ const Index = () => {
                 <HeaderSocials/>
            
 
-                <a href="#Contact" className="btn">Hire Me</a>
+                <a href="#contact" className="btn">Hire Me</a>
             </div>
         </section>
     )
